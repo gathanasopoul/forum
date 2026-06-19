f924d644f6848ae30c7da279f04bf5e27a552525
cpaloglo <Xristos_AX7@hotmail.com>
2026-06-19 14:14:21 +0300

    Add post reaction service

--- internal/services/reaction_service.go
+++ internal/services/reaction_service.go
@@ -1,1 +1,32 @@
 package services
+
+import (
+	"database/sql"
+)
+
+// AddPostReaction creates a like or dislike for a post.
+func AddPostReaction(
+	db *sql.DB,
+	userID int,
+	postID int,
+	value int,
+) error {
+
+	query := `
+	INSERT INTO post_reactions (
+		user_id,
+		post_id,
+		value
+	)
+	VALUES (?, ?, ?)
+	`
+
+	_, err := db.Exec(
+		query,
+		userID,
+		postID,
+		value,
+	)
+
+	return err
+}
